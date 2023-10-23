fb838c6cf1c6756297d44356680626a214a94bd9
medhosni <mohamed.hosni.isi@gmail.com>
2023-10-23 15:05:34 +0100

    Layout

---
+++ app/src/main/java/com/example/testesb/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.testesb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+        setContentView(R.layout.relative_layout);
+        setContentView(R.layout.constraint_layout);
+    }
+}
